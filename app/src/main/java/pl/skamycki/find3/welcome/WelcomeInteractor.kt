7ed42dc987f51d54a052f76e73daae0ffb011ec3
Szymon Kamycki <szymon.kamycki@gmail.com>
2018-08-26 02:55:30 +0200

    Improved navigation between welcome screen fields.

--- app/src/main/java/pl/skamycki/find3/welcome/WelcomeInteractor.kt
+++ app/src/main/java/pl/skamycki/find3/welcome/WelcomeInteractor.kt
@@ -1,5 +1,6 @@
 package pl.skamycki.find3.welcome
 
+import android.view.inputmethod.EditorInfo
 import pl.skamycki.find3.app.settings.AppSettings
 import javax.inject.Inject
 
@@ -8,6 +9,27 @@ class WelcomeInteractor @Inject constructor(
         private val navigation: WelcomeNavigation
 ) {
     fun onSaveClicked(
+            family: String,
+            device: String,
+            url: String
+    ) = handleSave(
+            family,
+            device,
+            url
+    )
+
+    fun onKeyboardAction(actionId: Int,
+                         family: String,
+                         device: String,
+                         url: String) = when (actionId) {
+        EditorInfo.IME_ACTION_DONE, EditorInfo.IME_ACTION_NEXT -> {
+            handleSave(family, device, url)
+            true
+        }
+        else -> false
+    }
+
+    private fun handleSave(
             family: String,
             device: String,
             url: String
